1004ce6aead4f32d695917536570faed2f489a67
최정민 <dndbejtk@naver.com>
2021-09-04 23:45:11 +0900

    각각의 대(소)문자 -> 소(대)문자로 변환하기

--- practiceAlgorithm/src/algorithm/UpperAndLowerConversion.java
+++ practiceAlgorithm/src/algorithm/UpperAndLowerConversion.java
@@ -9,8 +9,8 @@ public class UpperAndLowerConversion {
 		// result = HELLOwORLd
 		
 		String inputWord = "helloWorlD"; // 입력값 선언
-		char[] arr = inputWord.toCharArray(); // toCharArray() : String타입의 문자열을 한 글자씩 쪼개서 
-											  // 				 char타입의 배열에 넣어주는 메서드
+		char[] arr = inputWord.toCharArray(); // toCharArray() : String타입의 문자열을 한 글자씩 쪼개서 char타입의 배열에 넣어주는 메서드
+		
 		System.out.print("변환 전 : ");
 		System.out.print(arr);
 		
