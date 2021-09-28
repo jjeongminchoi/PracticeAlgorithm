bf9d269fb5f07b5365ca19fac4733aa62211c5af
Jeong-Min CHOI <76789355+jjeongminchoi@users.noreply.github.com>
2021-09-28 23:09:12 +0900

    Update _39_OneTwoThreeSum.java

--- practiceAlgorithm/src/algorithm/_39_OneTwoThreeSum.java
+++ practiceAlgorithm/src/algorithm/_39_OneTwoThreeSum.java
@@ -28,12 +28,14 @@ public class _39_OneTwoThreeSum {
 	static int cache[] = new int[1000];
 	
 	public static int doFunc(int n) {
+
 		if(cache[n] != 0) {
 			return cache[n];
 		}
 		
 		// [n] = [n-1] + [n-2] + [n-3] (점화식(DP)
 		return cache[n] = doFunc(n-1) + doFunc(n-2) + doFunc(n-3);
+
 	}
 	
 	public static void main(String[] args) {
